bd7e000b25da26cd00e8924a620ec7d7171efe0a
Minwoo Park <positive1attraction@gmail.com>
2021-04-17 15:53:49 +0900

    Attaching access restrictors to parameters in the constructor

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import * as CryptoJS from "crypto-js";
 import { timeStamp } from "node:console";
 
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public timestamp: number;
-  public data: string;
+  // public index: number;
+  // public hash: string;
+  // public previousHash: string;
+  // public timestamp: number;
+  // public data: string;
 
   static calculateBlockHash = (
     index: number,
@@ -24,17 +24,17 @@ class Block {
     typeof aBlock.data === "string";
 
   constructor(
-    index: number,
-    hash: string,
-    previousHash: string,
-    data: string,
-    timestamp: number
+    public index: number,
+    public hash: string,
+    public previousHash: string,
+    public data: string,
+    public timestamp: number
   ) {
-    (this.index = index),
-      (this.hash = hash),
-      (this.previousHash = previousHash),
-      (this.data = data),
-      (this.timestamp = timestamp);
+    // (this.index = index),
+    //   (this.hash = hash),
+    //   (this.previousHash = previousHash),
+    //   (this.data = data),
+    //   (this.timestamp = timestamp);
   }
 }
 
